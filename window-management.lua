55c25b8fd1bacd6d636bfa81f7f49555349d1329
Robin Wenglewski <robin@wenglewski.de>
2016-10-17 12:48:05 +0200

    clean up window-management bindings

--- window-management.lua
+++ window-management.lua
@@ -1,40 +1,27 @@
---------------------------------------------------------------------------------
--- CONSTANTS
---------------------------------------------------------------------------------
 local cmd = {'cmd'}
 local cmd_ctrl = {"cmd", "ctrl"}
 local cmd_alt = {"cmd", "alt"}
 local cmd_alt_ctrl = {"cmd", "alt", "ctrl"}
 
---------------------------------------------------------------------------------
--- CONFIGURATIONS
---------------------------------------------------------------------------------
-
 hs.window.animationDuration = 0
 hs.grid.setGrid'2x2'
 
---- Bindings
-
-hs.hotkey.bind(cmd_alt_ctrl, "R", function()
-    hs.reload()
-    hs.alert.show("Config loaded")
-end)
-
+-- move window to next screen
 hs.hotkey.bind(cmd_ctrl, "N", function()
 	local win = hs.window.focusedWindow()
 	win:moveToScreen(win:screen():next())
 end)
 
+-- maximize current window
 hs.hotkey.bind(cmd_ctrl, "F", function()
 	hs.window.focusedWindow():maximize()
 end)
 
--- Left 1/2 of the screen
+-- 1/2 of the screen bindings
 hs.hotkey.bind(cmd_ctrl, "left", function()
 	hs.grid.set(hs.window.focusedWindow(), { x=0, y=0, w=1, h=2 })
 end)
 
--- Right 1/2 of the screen
 hs.hotkey.bind(cmd_ctrl, "right", function()
 	hs.grid.set(hs.window.focusedWindow(), { x=1, y=0, w=1, h=2 })
 end)

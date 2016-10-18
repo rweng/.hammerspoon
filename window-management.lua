5f0cc5a5e4b8d546f99f16274dc91c95b965ee53
Robin Wenglewski <robin@wenglewski.de>
2016-10-18 13:38:25 +0200

    window management: modify up and down to work with full and half width

--- window-management.lua
+++ window-management.lua
@@ -1,3 +1,5 @@
+local logger = hs.logger.new('window-management.lua', 'info')
+
 local cmd = {'cmd'}
 local cmd_ctrl = {"cmd", "ctrl"}
 local cmd_alt = {"cmd", "alt"}
@@ -26,10 +28,16 @@ hs.hotkey.bind(cmd_ctrl, "right", function()
 	hs.grid.set(hs.window.focusedWindow(), { x=1, y=0, w=1, h=2 })
 end)
 
+-- up and down behave differently in the way that they keep the width and x position,
+-- but push the window in the upper or lower half of the screen
 hs.hotkey.bind(cmd_ctrl, "up", function()
-	hs.grid.set(hs.window.focusedWindow(), { x=0, y=0, w=2, h=1 })
+	local win = hs.window.focusedWindow()
+    local cell = hs.grid.get(win)
+	hs.grid.set(hs.window.focusedWindow(), { x=cell.x, y=0, w=cell.w, h=1 })
 end)
 
 hs.hotkey.bind(cmd_ctrl, "down", function()
-	hs.grid.set(hs.window.focusedWindow(), { x=0, y=1, w=2, h=1 })
+	local win = hs.window.focusedWindow()
+    local cell = hs.grid.get(win)
+	hs.grid.set(hs.window.focusedWindow(), { x=cell.x, y=1, w=cell.w, h=1 })
 end)

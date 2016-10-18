d346c49a61e9e6a490d4215b7f7ccbb639b41baf
Robin Wenglewski <robin@wenglewski.de>
2016-10-18 12:22:47 +0200

    map arrow keys in hyper mode

--- hyper.lua
+++ hyper.lua
@@ -1,46 +1,43 @@
-inspect = require('inspect')
-logger = hs.logger.new('My Settings', 'info')
+local inspect = require('inspect')
+local logger = hs.logger.new('hyper.lua', 'info')
 
 -- A global variable for the Hyper Mode
-k = hs.hotkey.modal.new({}, "F17")
+local k = hs.hotkey.modal.new()
+local hyperKey = 'F18'
 
 -- Enter Hyper Mode when F18 (Hyper/Capslock) is pressed
-pressedF18 = function(modifiers)
-  fn = function()
-    k.modifiers = modifiers
-    k.triggered = false
-    k:enter()
-  end
-  return fn
+local pressedHyper = function()
+  k.modifiers = modifiers
+  k.triggered = true
+  k:enter()
 end
 
 -- Leave Hyper Mode when F18 (Hyper/Capslock) is pressed,
 --   send ESCAPE if no other keys are pressed.
-releasedF18 = function()
+local releasedHyper = function()
   k:exit()
   if not k.triggered then
     hs.eventtap.keyStroke({}, 'ESCAPE')
   end
 end
 
--- Bind the Hyper key
--- hs.hotkey.bind(nil, 'F18', pressedF18({}), releasedF18)
-hs.hotkey.bind({'cmd', 'alt'}, 'F18', pressedF18({'cmd'}), releasedF18)
-
-movements = {
- { 'i', 'up'},
- { 'j', 'left'},
- { 'k', 'down'},
- { 'l', 'right'}
+local modifierCombinations = {
+  {}, {'cmd'}, {'alt'}, {'shift'}, {'ctrl'},
+  {'cmd', 'ctrl'}, {'cmd', 'alt'}, {'cmd', 'shift'}, {'ctrl', 'alt'}, {'ctrl', 'shift'}, {'alt', 'shift'},
+  {'cmd', 'ctrl', 'alt'}, {'cmd', 'ctrl', 'shift'}, {'ctrl', 'alt', 'shift'},
+  {'cmd', 'ctrl', 'alt', 'shift'}
 }
 
-for i,bnd in ipairs(movements) do
-  k:bind({}, bnd[1], function() 
-    logger.i('pressed', inspect(k.modifiers))
-    hs.eventtap.keyStroke(k.modifiers,bnd[2])
-  end)
-  k:bind({'cmd'}, bnd[1], function() 
-    logger.i('pressed cmd', inspect(k.modifiers))
-    hs.eventtap.keyStroke(k.modifiers,bnd[2])
-  end)
+local function left(modifiers) return function() hs.eventtap.keyStroke(modifiers, "left") end end
+local function right(modifiers) return function() hs.eventtap.keyStroke(modifiers, "right") end end
+local function up(modifiers) return function() hs.eventtap.keyStroke(modifiers, "up") end end
+local function down(modifiers) return function()hs.eventtap.keyStroke(modifiers, "down") end end
+
+-- Bind the Hyper and arrow keys
+for i, modifiers in ipairs(modifierCombinations) do
+  hs.hotkey.bind(modifiers, hyperKey, pressedHyper, releasedHyper)
+  k:bind(modifiers, 'j', left(modifiers))
+  k:bind(modifiers, 'l', right(modifiers))
+  k:bind(modifiers, 'i', up(modifiers))
+  k:bind(modifiers, 'k', down(modifiers))
 end
